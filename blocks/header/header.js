e0adf5e866b846f0e59588f69519c84fdb6a5db6
Lamont Crook <lamont@adobe.com>
2023-02-24 11:58:18 -0700

    update

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -124,6 +124,19 @@ export default async function decorate(block) {
       });
     }
 
+    //search
+    const search = nav.querySelector('.icon-search');
+    const searchForm = document.createElement('form'); //createTag('form', {class: 'no-submit'});
+    const searchContainer = document.createElement('div'); 
+    searchContainer.classList.add('search-container');
+    searchContainer.append(searchForm);
+    console.log(searchContainer);
+    //search.append(searchContainer);
+    searchContainer.append(search);
+    nav.querySelector('.nav-brand > p:nth-child(2)').replaceWith(searchContainer);
+   
+    console.log(nav.querySelector('.nav-brand'));
+
     // hamburger for mobile
     const hamburger = document.createElement('div');
     hamburger.classList.add('nav-hamburger');
